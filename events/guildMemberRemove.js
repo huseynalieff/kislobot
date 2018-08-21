5caf68dd7da44078cd22fdf794e76d620b3b590c
huseynalieff <huseyn.alieff@gmail.com>
2018-08-21 21:14:03 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,5 @@
 module.exports = member => {
-  let guild = member.guild;
-  member.sendMessage('niye gittin?');
-  guild.defaultChannel.sendMessage(`${member.user.username} gitti.`);
+    let username = member.user.username;
+    member.sendMessage('Hoşçakal **' + username + '**!');
+    member.guild.defaultChannel.send('görüşürüz '+username+'');
 };
