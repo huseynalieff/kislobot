028da48701f6f36d2ffad35e69864536cc479bb2
huseynalieff <huseyn.alieff@gmail.com>
2018-08-21 21:09:28 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,7 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send('hg '+username+'');
-};
+client.on(""" " , (=> {
+ const guild  =member.guild.newUsers(set.member, id.member);
+
+ user (if.newUsers10 ) {
+  10) {
+ = defaultChannel . guild.channels(find=>c. c(permissionsFor).guild).("(" SEND_MESSAGES"));
+ = userlist . newUsers(map=>u . u().toString("" " <  ".defaultChannel("(""\n" .
