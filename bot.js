409a189d43a2b230a2192e39f6e1be4338c5cec8
huseynalieff <huseyn.alieff@gmail.com>
2018-08-21 21:11:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,24 +159,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-
-client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-  newUsers[guild.id].set(member.id, member.user);
-
-  if (newUsers[guild.id].size > 10) {
-    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-    guild.channels.find("name", "genel").send("Welcome our new users!\n" + userlist);
-    newUsers[guild.id].clear();
-  }
-});
-
-client.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
-});
-
-
-
 client.login(process.env.BOT_TOKEN);
