2fe7bf2932f2e4bec162f949da294b916b9d4d77
huseynalieff <huseyn.alieff@gmail.com>
2018-08-21 22:19:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,18 +160,4 @@ client.on('error', e => {
 });
 
 
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'hoşgeldiniz');
-  if (!channel) return;
-  const embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setAuthor(member.user.username, member.user.avatarURL)
-  .setThumbnail(member.user.avatarURL)
-  .setTitle('📤 | Sunucuya Katıldı | Hoşgeldin ')
-  .setTimestamp()
-  channel.sendEmbed(embed);
-});
-
-
-
 client.login(process.env.BOT_TOKEN);
