9aad12dc2a51eacbafbff2d063974c2c0078a77c
huseynalieff <huseyn.alieff@gmail.com>
2018-08-21 14:11:53 +0300

    Update çay.js

--- komutlar/çay.js
+++ komutlar/çay.js
@@ -4,14 +4,18 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
  
     const embed = new Discord.RichEmbed()
-	    .setAuthor(message.author.username, message.author.avatarURL)
+	    .setAuthor(message.author.username)
         .setDescription("**GEL ÇAYIMIN ŞEKERİ**")
         .setImage("https://i2.wp.com/www.oyunkaynagi.com/wp-content/uploads/2017/09/cropped-cay-cay-bardagi-kahvalti-cay-simit-incebelli-bardak-resimleri-V070220151622-N-21.png?ssl=1")
         .setThumbnail("https://i2.wp.com/www.oyunkaynagi.com/wp-content/uploads/2017/09/cropped-cay-cay-bardagi-kahvalti-cay-simit-incebelli-bardak-resimleri-V070220151622-N-21.png?ssl=1")
         .setColor(0x00AE86)
         .addField("Demişlerki", "Herşeyi salla ama ÇAYI demle hacı", true)
-   
-   .addField("Oksijeni (Kislo) bilmem ama yaşamak için çay şart :D", "")
+        .addField("Yetenekleri", `
+        *Havanın hayatımızdaki en büyük rolünü
+   *Anlatmaya gerek yok bence :D Discorddada bu geçerlidir ;)
+   `, true)
+   .addField("Sevdiği İş", `Banlamak :D`, true)
+   .addField("Oksijeni (Kislo) bilmem ama yaşamak için çay şart", "Kurabiyeler sende")
    
 
    message.channel.send(embed)
