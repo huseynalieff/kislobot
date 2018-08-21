34a9c56e137c3051d73f45c281c090e6f523863e
huseynalieff <huseyn.alieff@gmail.com>
2018-08-21 14:15:15 +0300

    Update çay.js

--- komutlar/çay.js
+++ komutlar/çay.js
@@ -4,18 +4,18 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
  
     const embed = new Discord.RichEmbed()
-	    .setAuthor(message.author.username)
+	    .setAuthor(message.author.username, message.author.avatarURL)
         .setDescription("**GEL ÇAYIMIN ŞEKERİ**")
         .setImage("https://i2.wp.com/www.oyunkaynagi.com/wp-content/uploads/2017/09/cropped-cay-cay-bardagi-kahvalti-cay-simit-incebelli-bardak-resimleri-V070220151622-N-21.png?ssl=1")
         .setThumbnail("https://i2.wp.com/www.oyunkaynagi.com/wp-content/uploads/2017/09/cropped-cay-cay-bardagi-kahvalti-cay-simit-incebelli-bardak-resimleri-V070220151622-N-21.png?ssl=1")
         .setColor(0x00AE86)
         .addField("Demişlerki", "Herşeyi salla ama ÇAYI demle hacı", true)
-        .addField("Yetenekleri", `
-        *Havanın hayatımızdaki en büyük rolünü
-   *Anlatmaya gerek yok bence :D Discorddada bu geçerlidir ;)
+        .addField("Oğuz Atay", `
+        *biz, çayın yalnızlığa iyi gelen tarafını da severiz. Avuçlarken ince belli bardağı,
+   *hücrelere kadar hissettiren sıcaklığında unuttuk yalnızlığı.
    `, true)
-   .addField("Sevdiği İş", `Banlamak :D`, true)
-   .addField("Oksijeni (Kislo) bilmem ama yaşamak için çay şart", "Kurabiyeler sende")
+   
+   .addField("Oksijeni (Kislo) bilmem ama yaşamak için çay şart :D", "")
    
 
    message.channel.send(embed)
