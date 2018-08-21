9db99cd8488a481bcc067bc37e5e45189602412d
huseynalieff <huseyn.alieff@gmail.com>
2018-08-21 14:18:10 +0300

    Update çay.js

--- komutlar/çay.js
+++ komutlar/çay.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
  
     const embed = new Discord.RichEmbed()
-	    .setAuthor(message.author.username, message.author.avatarURL)
+	    .setAuthor(message.author.username)
         .setDescription("**GEL ÇAYIMIN ŞEKERİ**")
         .setImage("https://i2.wp.com/www.oyunkaynagi.com/wp-content/uploads/2017/09/cropped-cay-cay-bardagi-kahvalti-cay-simit-incebelli-bardak-resimleri-V070220151622-N-21.png?ssl=1")
         .setThumbnail("https://i2.wp.com/www.oyunkaynagi.com/wp-content/uploads/2017/09/cropped-cay-cay-bardagi-kahvalti-cay-simit-incebelli-bardak-resimleri-V070220151622-N-21.png?ssl=1")
