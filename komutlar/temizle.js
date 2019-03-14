b4ab4416ee50a8fe40abfa623845ccecbde8c6fd
Hüseyn Aliyev <huseyn.alieff@gmail.com>
2019-03-14 14:55:45 +0400

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,10 +1,29 @@
 const Discord = require('discord.js');
 
 
-exports.run = function(client, message) {
-message.channel.bulkDelete(20);
-message.channel.send("20 mesaj sildim :muscle: ").then(msg => {
-	msg.delete(5000)
+exports.run = function(client, message, args) {
+
+    if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`temizle` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
+  
+      if (isNaN(args[0])) {
+            message.reply(":warning: Kaç mesaj sileceğimi belirtmedin.")
+            return
+        }
+        
+        if (args[0].length > 99) {
+            message.channel.send(":warning: 99'dan fazla mesaj silemem.")
+            return
+        }
+message.channel.bulkDelete(args[0]);
+message.channel.send(args[0]+" adet mesaj sildim :muscle: ").then(msg => {
+  msg.react("✅")
+    msg.delete(5000)
 })
 
 };
@@ -18,6 +37,6 @@ exports.conf = {
 
 exports.help = {
   name: 'temizle', 
-  description: 'Belirtilen miktarda mesaj siler. Sadece sunucu sahibi ve moderatörler',
+  description: 'Belirtilen miktarda mesaj siler',
   usage: 'temizle <miktar>'
 };
