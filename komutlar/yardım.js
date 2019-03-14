dba835d70142f3f81015eda335917c37efddcc7f
Hüseyn Aliyev <huseyn.alieff@gmail.com>
2019-03-14 14:59:12 +0400

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,38 +1,29 @@
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const Discord = require("discord.js");
 
-var prefix = ayarlar.prefix;
+module.exports.run = async (bot, message, args) => {
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("**:page_facing_up: Kislo Yardım Menüsü**")
+    .setColor("#15f153")
+    .addField("k-eğlence", "Eğlence komutlarını gösterir")
+	.addField("k-eğlence2", "Eğlence2 komutlarını gösterir")
+    .addField("k-kullanıcı:", "Kullanıcı komutlarını gösterir")
+    .addField("k-yetkili:", "Yetkili komutlarını gösterir")
+    .addField("k-sahip:", "Sahiplerin kullanabileceği komutları gösterir")
+    message.react("🇹")
+      message.react("🇲")
 
-exports.run = (client, message, params) => {
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
-  if (message.channel.type !== 'dm') {
-    const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Özel mesajlarını kontrol et. :postbox:');
-    message.channel.sendEmbed(ozelmesajkontrol) }
-  } else {
-    let command = params[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
-    }
-  }
-};
+    message.channel.send(serverembed);
+}
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'y'],
+  aliases: ['h'],
   permLevel: 0
 };
 
 exports.help = {
   name: 'yardım',
-  description: 'Tüm komutları gösterir.',
-  usage: 'yardım [komut]'
+  description: 'Bütün komutları verir.',
+  usage: 'yardım'
 };
